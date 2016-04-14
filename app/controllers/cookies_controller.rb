d5f81e201a1bf2e1cf56a6b4cb2b64bed698bbe3
ardeshirj <ardeshir.javaherchi@gmail.com>
2016-04-13 19:11:14 -0700

    Add pusher auth method & configs

--- app/controllers/cookies_controller.rb
+++ app/controllers/cookies_controller.rb
@@ -1,2 +1,19 @@
+# Pusher controller
 class CookiesController < ApplicationController
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :null_session,
+                       if: proc { |c| c.request.format == 'application/json' }
+
+  def auth
+    if current_user
+      response = Pusher[params[:channel_name]].authenticate(
+        params[:socket_id],
+        user_id: current_user.id
+      )
+      render json: response
+    else
+      render text: 'Forbidden', status: '403'
+    end
+  end
 end
---
+++ config/initializers/pusher.rb
@@ -0,0 +1,7 @@
+require 'pusher'
+
+Pusher.app_id = '197008'
+Pusher.key = '885cb9dcf5e442cbfe46'
+Pusher.secret = '69f23e590bb74591b921'
+Pusher.logger = Rails.logger
+Pusher.encrypted = true
