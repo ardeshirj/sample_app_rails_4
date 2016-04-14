8665f5264f25387c22d133a9613431244b69c921
ardeshirj <ardeshir.javaherchi@gmail.com>
2016-04-13 18:09:08 -0700

    Add auth route

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ SampleApp::Application.routes.draw do
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
+  match '/auth',    to: 'cookies#auth',         via: 'post'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
